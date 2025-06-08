da7e6b687278056d6b1483542d1dba7cea1d136b
Rakesh-592 <rakeshkunapareddy333@gmail.com>
2025-06-08 13:48:31 +0530

    made changes for render deploy

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+// mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const userSchema = mongoose.Schema({
   username: String,
