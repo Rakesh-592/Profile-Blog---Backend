77f0e36715b03fa021c749f3fb84df5244213eb2
Rakesh-592 <rakeshkunapareddy333@gmail.com>
2025-06-08 12:32:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,150 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const upload = require("./config/multerconfig");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", async (req, res) => {
+  let { email, password, username, age, name } = req.body;
+  let user = await userModel.findOne({ email });
+
+  if (user) return res.status(500).send("User already registered");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username,
+        password: hash,
+        email,
+        age,
+        name,
+      });
+
+      let token = jwt.sign(
+        { email: email, userid: user._id },
+        process.env.JWT_SECRET
+      );
+      res.cookie("token", token);
+      res.send("Registered");
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email });
+
+  if (!user) return res.status(500).send("Something went wrong");
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign(
+        { email: email, userid: user._id },
+        process.env.JWT_SECRET
+      );
+      res.cookie("token", token);
+      res.redirect("/profile"); // res.status(200).send("you can login");
+    } else res.redirect("/login");
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("posts");
+  console.log(req.user);
+  res.render("profile", { user });
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1); //removing like
+  }
+
+  await post.save();
+  res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  res.render("edit", { post });
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content }
+  );
+
+  res.redirect("/profile");
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+});
+
+app.get("/profile/upload", (req, res) => {
+  res.render("profileupload");
+});
+
+app.post("/upload", isLoggedIn, upload.single("image"), async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.profilepic = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
+});
+
+//middleware
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "")
+    res.redirect("/login"); // res.status(201).send("You must be logged in");
+  else {
+    let data = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    req.user = data;
+    next();
+  }
+  // console.log(req.cookies);
+}
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
